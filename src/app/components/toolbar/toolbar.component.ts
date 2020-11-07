d9233949970536425bceaa74a95c5ae46f4b5644
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-07 23:56:47 +0000

    Passing titles

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent implements OnInit {
+export class ToolbarComponent {
 
-  constructor() { }
+  @Input() public title = '';
 
-  ngOnInit(): void {
-  }
 
 }
