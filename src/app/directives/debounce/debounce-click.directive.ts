d9d5a0e81ae1fa00d4c60b58d838a16af13016d1
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 16:07:12 +0000

    Events added and unsubscribe

--- src/app/directives/debounce/debounce-click.directive.ts
+++ src/app/directives/debounce/debounce-click.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
 
 @Directive({
-  selector: '[appDebounceClick]'
+  selector: '[appDebounceClick]',
 })
-export class DebounceClickDirective {
+export class DebounceClickDirective implements OnInit, OnDestroy {
+  @Output() public debounceclick = new EventEmitter();
+  public clicks = new Subject();
+  public sub$: Subscription;
 
-  constructor() { }
+  public ngOnInit(): void {
+    const debounceTimeinMs = 500;
+    this.sub$ = this.clicks
+      .pipe(debounceTime(debounceTimeinMs))
+      .subscribe((value: any) => this.debounceclick.emit(value));
+  }
 
+  public ngOnDestroy(): void {
+    this.sub$.unsubscribe();
+  }
 }
