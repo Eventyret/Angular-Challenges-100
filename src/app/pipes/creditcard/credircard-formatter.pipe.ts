d64ea319e260400566581a116b272d4664c9f8a4
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 13:09:18 +0000

    added formatter

--- src/app/pipes/creditcard/credircard-formatter.pipe.ts
+++ src/app/pipes/creditcard/credircard-formatter.pipe.ts
@@ -11,7 +11,9 @@ export class CredircardFormatterPipe implements PipeTransform {
     if (!this.isAllNumbers(cardNumber)) {
       return 'Invalid Characters';
     }
+    return this.formatCardNumber(cardNumber);
   }
+
   private hasCorrectLength(cardNumber: string): boolean {
     const cardNumberLength = cardNumber.length;
     const isMasterDiscoverVisaCardLength = 16;
@@ -25,11 +27,17 @@ export class CredircardFormatterPipe implements PipeTransform {
     }
     return false;
   }
+
   private isAllNumbers(cardNumber: string): boolean {
     const stringNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     const totalValidCharacters = cardNumber
       .split('')
       .filter((char) => stringNumbers.includes(char)).length;
     return totalValidCharacters === cardNumber.length;
+  }
+
+  private formatCardNumber(cardNumber: string): string {
+    const parts = cardNumber.match(/[\s\S]{1,4}/g);
+    return parts.join('-');
   }
 }
