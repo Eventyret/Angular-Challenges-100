a4ef3400975d3e61173f79f5a69fc0c94a2aad1c
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 12:39:40 +0000

    Added loader Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../components/card/card.component';
+import { LoaderComponent } from '../components/loader/loader.component';
 
 
 @NgModule({
   declarations: [
     CardComponent,
+    LoaderComponent
   ],
   exports: [
     CardComponent,
+    LoaderComponent
   ],
   imports: [CommonModule],
 })
