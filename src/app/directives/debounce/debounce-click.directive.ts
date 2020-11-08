e931ea2ac09d28fd38002c2305316f6d3941df2f
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 16:01:56 +0000

    Created debounce directive

---
+++ src/app/directives/debounce/debounce-click.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appDebounceClick]'
+})
+export class DebounceClickDirective {
+
+  constructor() { }
+
+}
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { DIRECTIVE_ROUTES} from './directives.routes';
 import { DirectiveDocumentationComponent } from './directive-documentation/directive-documentation.component';
+import { DebounceClickDirective } from './debounce/debounce-click.directive';
 
 
 @NgModule({
-  declarations: [DirectiveDocumentationComponent],
+  declarations: [DirectiveDocumentationComponent, DebounceClickDirective],
   imports: [CommonModule, RouterModule.forChild(DIRECTIVE_ROUTES)],
 })
 export class DirectivesModule {}
