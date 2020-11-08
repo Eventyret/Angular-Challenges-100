648bded522504cb40e8a6e72d5223bb8053c0016
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 09:09:33 +0000

    Created half star logic

--- src/app/components/star-rating/star-rating.component.ts
+++ src/app/components/star-rating/star-rating.component.ts
@@ -3,13 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-star-rating',
   templateUrl: './star-rating.component.html',
-  styleUrls: ['./star-rating.component.scss']
+  styleUrls: ['./star-rating.component.scss'],
 })
-export class StarRatingComponent  {
+export class StarRatingComponent {
   @Input() public rating = 5;
 
   public get fullStars(): number[] {
     const totalFullStars = Math.floor(this.rating);
     return Array(totalFullStars).fill(0);
+  }
+  public get hasHalfStar(): boolean {
+    const hasHalfStar =
+      this.rating - Math.floor(this.rating) > 0.5 && this.rating !== 5;
+    return hasHalfStar;
   }
 }
