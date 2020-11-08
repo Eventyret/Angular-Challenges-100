095fa7533c95888858af442d148bf4c69e76d658
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 09:42:47 +0000

    Completed logic

--- src/app/components/top-of-page/top-of-page.component.ts
+++ src/app/components/top-of-page/top-of-page.component.ts
@@ -1,5 +1,5 @@
 import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: 'app-top-of-page',
@@ -7,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-of-page.component.scss'],
 })
 export class TopOfPageComponent {
+  @Input() public pxToShow = 400;
+  public isShown = false;
   constructor(private viewPortScroller: ViewportScroller) {}
+
+  @HostListener('window:scroll') onWindowScroll(): void {
+    const yCoordinate = this.viewPortScroller.getScrollPosition()[1];
+    this.isShown = yCoordinate >= this.pxToShow;
+  }
 
   public goToTop(): void {
     this.viewPortScroller.scrollToPosition([0, 0]);
