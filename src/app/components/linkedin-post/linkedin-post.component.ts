851314e7b7f8634c8b8a92913773af7b9bd8c522
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 15:13:22 +0000

    Added get method

--- src/app/components/linkedin-post/linkedin-post.component.ts
+++ src/app/components/linkedin-post/linkedin-post.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-linkedin-post',
   templateUrl: './linkedin-post.component.html',
-  styleUrls: ['./linkedin-post.component.scss']
+  styleUrls: ['./linkedin-post.component.scss'],
 })
-export class LinkedinPostComponent  {
-
-
+export class LinkedinPostComponent {
+  constructor(public router: Router) {}
+  public get linkedInMessage(): string {
+    return '';
+  }
 }
