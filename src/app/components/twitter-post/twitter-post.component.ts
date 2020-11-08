5bdb0f38283740b1b5a46c33fe1a17eaf80a87a2
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 14:37:44 +0000

    Added inpouts and get methods

--- src/app/components/twitter-post/twitter-post.component.ts
+++ src/app/components/twitter-post/twitter-post.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-twitter-post',
   templateUrl: './twitter-post.component.html',
-  styleUrls: ['./twitter-post.component.scss']
+  styleUrls: ['./twitter-post.component.scss'],
 })
-export class TwitterPostComponent implements OnInit {
+export class TwitterPostComponent {
+  @Input() public baseHref = 'https://dehlin.dev';
+  @Input() public hashTags: string[] = [
+    'Simen Daehlin',
+    'Javascript',
+    'TypeScript',
+    'Angular',
+    '100 Angular Challanges',
+  ];
 
-  constructor() { }
+  constructor(public titleService: Title) {}
 
-  ngOnInit(): void {
+  public get twitterMessage(): string {
+    return '';
   }
-
 }
