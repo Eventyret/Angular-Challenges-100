57e0ce730bfe01408a629f4d1b29399b07d5eee1
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 09:29:17 +0000

    Added first logic for top of page logic

--- src/app/components/top-of-page/top-of-page.component.ts
+++ src/app/components/top-of-page/top-of-page.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-of-page',
   templateUrl: './top-of-page.component.html',
-  styleUrls: ['./top-of-page.component.scss']
+  styleUrls: ['./top-of-page.component.scss'],
 })
-export class TopOfPageComponent implements OnInit {
+export class TopOfPageComponent {
+  constructor(private viewPortScroller: ViewportScroller) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public goToTop(): void {
+    this.viewPortScroller.scrollToPosition([0, 0]);
   }
-
 }
