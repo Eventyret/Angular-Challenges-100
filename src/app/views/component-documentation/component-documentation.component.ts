1e0b70fd6f52a94de08c361faabd97d9b9007d73
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 10:01:17 +0000

    Moved from app to different views

--- src/app/views/component-documentation/component-documentation.component.ts
+++ src/app/views/component-documentation/component-documentation.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AccordianItem } from 'src/app/models/accordian.models';
 
 @Component({
   selector: 'app-component-documentation',
   templateUrl: './component-documentation.component.html',
-  styleUrls: ['./component-documentation.component.scss']
+  styleUrls: ['./component-documentation.component.scss'],
 })
-export class ComponentDocumentationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ComponentDocumentationComponent  {
+  public accoridanItems: AccordianItem[] = [
+    {
+      title: 'Example 1',
+      content: 'Example Content 1',
+      isExpanded: false,
+    },
+    {
+      title: 'Example 2',
+      content: 'Example Content 2',
+      isExpanded: false,
+    },
+  ];
+  public progressValue = 25;
 }
