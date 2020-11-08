92becd0cbe55bffeac946ad4140e4ef18051dc43
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 13:25:46 +0000

    Logic for formatting and xxxx out

--- src/app/components/credit-card-input/credit-card-input.component.ts
+++ src/app/components/credit-card-input/credit-card-input.component.ts
@@ -10,9 +10,22 @@ export class CreditCardInputComponent {
 
   @Input() public creditCardNumber = '';
   @Input() public set isReadOnly(value: boolean) {
-    this.readOnly = value;
+    this.creditCardNumber = value
+      ? this.formatReadOnlyCCNum(this.creditCardNumber)
+      : this.creditCardNumber;
   }
   public get isReadOnly(): boolean {
     return this.readOnly;
+  }
+
+  private formatReadOnlyCCNum(ccNum: string): string {
+    const parts = ccNum.match(/[\s\S]{1,4}/g) || [];
+    const onlyLastFourShown = parts.map((part, index) => {
+      if (index === parts.length - 1) {
+        return part;
+      }
+      return 'xxxx';
+    });
+    return onlyLastFourShown.join('-');
   }
 }
