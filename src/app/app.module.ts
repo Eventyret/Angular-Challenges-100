857ab1481800087c0b6226f66902d8e90d2f663e
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 11:26:22 +0000

    Created shared module and components module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
-import { CardComponent } from './components/card/card.component';
-import { AccordianComponent } from './components/accordian/accordian.component';
-import { ProgressbarComponent } from './components/progressbar/progressbar.component';
-import { StarRatingComponent } from './components/star-rating/star-rating.component';
 import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentDocumentationComponent,
-    CardComponent,
-    AccordianComponent,
-    ProgressbarComponent,
-    StarRatingComponent,
     TopOfPageComponent,
     ToolbarComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES), SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PipeDocumentationComponent} from './pipe-documentation/pipe-documentation.component';
+import { SharedModule} from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { PIPES_ROUTES} from './pipes.routes';
 
@@ -10,6 +11,7 @@ import { PIPES_ROUTES} from './pipes.routes';
   declarations: [PipeDocumentationComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(PIPES_ROUTES)
   ]
 })
