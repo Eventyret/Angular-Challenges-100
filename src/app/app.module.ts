59ff11d12b0bb91df899f9d3f9314c4fbab8aab1
Simen Daehlin <simen.dehlin@gmail.com>
2020-11-08 01:04:13 +0000

    Added component and default styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordianComponent } from './components/accordian/accordian.component';
+import { ProgressbarComponent } from './components/progressbar/progressbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     CardComponent,
-    AccordianComponent
+    AccordianComponent,
+    ProgressbarComponent
   ],
   imports: [
     BrowserModule
